bcea20608fb97301698f903d1d5e3edb2ab690be
Nabha2004 <nabhabagwan32@gmail.com>
2025-03-23 19:51:09 +0530

    Basic setup done

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+main().then(()=>{
+    console.log("connected to DB");
+}).catch((err)=>{
+    console.log(err);
+});
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
+
+app.get("/",(req,res)=>{
+    res.send("Hi , I am root!");
+})
+
+let port = 3000;
+app.listen(port,()=>{
+    console.log(`server is listening to port ${port}`)
+})
