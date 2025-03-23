d9632f5e1b08a0d16be0815932fd2e1dd45ac049
Nabha2004 <nabhabagwan32@gmail.com>
2025-03-24 02:53:56 +0530

    delete route commited

--- app.js
+++ app.js
@@ -82,6 +82,14 @@ app.put("/listings/:id",async(req,res)=>{
     res.redirect(`/listings/${id}`);
 })
 
+//delete route
+app.delete("/listings/:id",async (req,res)=>{
+    let {id} = req.params; 
+    let deletedListing=await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+})
+
 
 let port = 3000;
 app.listen(port,()=>{
